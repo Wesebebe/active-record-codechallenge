30cf307714cdb7fff7fc383f5730247b1b5c52eb
+ <+>
2023-06-04 23:16:42 +0300

    Create delete method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ActiveRecord::Base
     def favorite_product 
         self.reviews.order(:star_rating).last
     end
+
+    def remove_reviews(product) 
+    self.reviews.where(product_id: product.id).destroy_all
+    end
 end
