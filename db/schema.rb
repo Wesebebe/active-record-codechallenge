53fd1404f1a4269798ec749dc7dcc26f2a294344
+ <+>
2023-06-04 22:04:36 +0300

    Create migration for the products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_04_185703) do
+ActiveRecord::Schema.define(version: 2023_06_04_190241) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
