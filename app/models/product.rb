b0440d5468bc8fa921942290aa1e755336dfb01a
+ <+>
2023-06-04 23:08:50 +0300

    Create leave review method

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
     has_many :reviews
     has_many :users, through: :reviews
+
+    def leave_review(user, star_rating, comment) 
+        Review.create(star_rating:star_rating, comment: comment, user_id:user.id, product_id: self.id)
+    end
 end
