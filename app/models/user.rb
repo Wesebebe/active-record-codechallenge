05c606a2ce13fe39943f4309c760cea06facc586
+ <+>
2023-06-04 23:15:07 +0300

    Create user favorite product method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
     has_many :reviews 
     has_many :products, through: :reviews 
+
+    def favorite_product 
+        self.reviews.order(:star_rating).last
+    end
 end
